d599f86e9888d4ab692b68a037037dbb8f51e154
Dongmin Kim <macaw2469@gmail.com>
2023-07-04 10:04:11 +0900

    반복문 학습

--- Practice/index.js
+++ Practice/index.js
@@ -1,23 +1,16 @@
-// 요거 파일을 제외하고 Add를 해야게쓔
 const fs = require('fs');
 
-const input = fs.readFileSync('example_input.txt').toString().split('\n');
+const input = fs.readFileSync("example_input.txt").toString().trim().split('\n')
 
-// 정렬정렬 김정렬 오름차순 김정렬
-
-const arr = input.map(item => Number(item.replace(/\r/g,"")))
-
-
-// 어떤 정렬을 써야하지? 일단은 알겠어
-// 일단 중복값을 지워야하는데 어케 지우누?
-// 일단은 한번 내 방식대로 해보고 증답 찾기
-
-// 어케 지우누... 뭐 구글링 하면 바로 나오겠지
-// 하지만 이건 알고리즘 인걸!! 생각 생각 생강차
-
-
-const test = arr.sort((next, pre)=>{
-    return next;
-})
-
-console.log(test)
+/** 여러 입력 케이스 출력
+ * 어떤 오류??? 왜 틀림?
+ * 0 0 이 들어 올때까지 ?
+ * !! trim()을 이용하여 양끝 공백을 제거해야함 
+ * -> 입력값 에러?
+*/
+let result = '';
+for(let i = 0 ; i < input.length-1 ; i++){
+    const nums = input[i].split(' ')
+    result += Number(nums[0]) + Number(nums[1]) + '\n'
+}
+console.log(result);
