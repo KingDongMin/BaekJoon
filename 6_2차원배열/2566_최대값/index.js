191e3281d230e2fd3b1580f346cac81cf6b1fecb
Dongmin Kim <macaw2469@gmail.com>
2023-07-30 21:25:31 +0900

    수정 : 2566_최대값

--- 6_2차원배열/2566_최대값/index.js
+++ 6_2차원배열/2566_최대값/index.js
@@ -12,7 +12,7 @@ let index = '';
 for(let i = 0 ; i < input.length ; i++){
   const arr = input[i].split(" ").map(el=>+el)
   for(let j = 0 ; j < arr.length ; j++ ){
-    if(result < arr[j]){
+    if(result <= arr[j]){
       result = arr[j];
       index= `${i+1} ${j+1}`;
     }
@@ -21,3 +21,6 @@ for(let i = 0 ; i < input.length ; i++){
 
 console.log(result);
 console.log(index);
+
+// 9 x 9 2차원 배열의 원소가 모두 0일때에도 행렬 index값이 나와야 한다
+// 이전 실패의 원인은 0일 경우 result 조건이 성립이 안되어 index의 초기값 ''공백이 출력되었다.
