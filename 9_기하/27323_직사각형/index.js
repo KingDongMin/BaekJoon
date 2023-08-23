93f8a4cc6f8363c9b8cc728c687c22d95c675cc6
Dongmin Kim <macaw2469@gmail.com>
2023-08-23 16:25:04 +0900

    학습 : 27323_직사각형

---
+++ 9_기하/27323_직사각형/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const input = fs.readFileSync('example_input.txt').toString().trim().split('\n');
+
+/** [ 27323 - 직사각형]
+ * - 세로 A, 가로 B인 직사각형의 넓이를 출력하라. 
+ */
+
+const A = Number(input[0]);
+const B = Number(input[1]);
+
+console.log(A*B)
+
