34f3607d5ed8c06da3102ab9bbfc15814941c44d
Dongmin Kim <macaw2469@gmail.com>
2023-09-02 17:39:31 +0900

    학습 : 24262_알고리즘의 수행 시간 1

---
+++ 10_시간복잡도/24262_알고리즘수행시간/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const input = fs.readFileSync('example_input.txt').toString().trim().split('\n');
+
+/**
+    [ 24262 - 알고리즘 수업 : 알고리즘의 수행 시간 1 ]
+    - 주어진 알고리즘 MeOfPassion의 수행횟수와 최고차항의 차수를 출력하여라
+
+    [ 사고 과정 ]
+    - 주어진 알고리즘은 입력값 n에 관계없이 한 번 수행된다.
+    - 빅오 표기법으론 O(1)이며 1의 차수는 0이다.
+ */
+
+    console.log(1)
+    console.log(0)
